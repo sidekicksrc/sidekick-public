80a2fe29cfc271fb18cb8d99f68d33a767f59383
Tim Ruffles <timruffles@googlemail.com>
2013-11-11 15:58:43 +0000

    new version

--- js/front.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".toggle").addEventListener("click",function(evt) {
-  evt.preventDefault();
-  console.log("clicked")
-  document.querySelector("nav").classList.toggle('active')
-})
