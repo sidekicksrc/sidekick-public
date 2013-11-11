97651f20abd5190e231be771428e59b614b58e18
Tim Ruffles <timruffles@googlemail.com>
2013-11-11 15:10:32 +0000

    updates

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 main() {
   set -e
   check_deployable
+  create_commit
   deploy
 }
 check_deployable() {
   diffs=`git diff --name-status HEAD | wc -l`
   if [[ $diffs != "0" ]]; then
-    echo "Can't deploy, unsaved changes:"
-    git diff --name-status HEAD
+    echo "Can't deploy, unsaved changes"
     exit 1
   fi
 }
@@ -20,18 +20,18 @@ deploy() {
 }
 
 most_recent_deploy() {
-  `git show-ref deploy-staging | awk '{print $2}'`
+  git show-ref deploy | awk '{print $1}'
 }
 
 create_commit() {
-  commit=most_recent_deploy
-  git add -f output
+  commit=`most_recent_deploy`
+  added=`git add -f output`
   tree=`git write-tree --prefix output/`
   parent_opt=""
   if [[ $commit != "" ]]; then
     parent_opt="-p $commit"
   fi
-  new_commit=git commit-tree $tree -m 'new version' $parent_opt
+  new_commit=`git commit-tree $tree -m 'new version' $parent_opt`
   git update-ref deploy $new_commit
 }
 
