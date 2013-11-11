ea806179881513bbd55b90110663c98d450a72d5
Tim Ruffles <timruffles@googlemail.com>
2013-11-11 12:14:04 +0000

    v1

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,41 @@
-
 main() {
   set -e
-  check_deployable()
-  deploy()
+  check_deployable
+  deploy
 }
 check_deployable() {
-  diffs=git diff --name-status HEAD | wc -l
+  diffs=`git diff --name-status HEAD | wc -l`
   if [[ $diffs != "0" ]]; then
     echo "Can't deploy, unsaved changes:"
     git diff --name-status HEAD
+    exit 1
   fi
 }
 
 deploy() {
   rm -rf output
-  checkout_clean_branch()
-  compile()
+  checkout_clean_branch
+  compile
   git add -f output
   git commit -m 'compiled'
   git checkout deploy
-  add_output()
+  create_commit
+  git push origin deploy
 }
 
-add_output() {
+most_recent_deploy() {
+  git show-ref deploy-staging | awk '{print $2}'
+}
+
+create_commit() {
+  commit=most_recent_deploy
   tree=git ls-tree -d | grep -w output | awk '{print $3}'
-  echo "Output folder tree" $tree
+  parent_opt=""
+  if [[ $commit != "" ]]; then
+    parent_opt="-p $commit"
+  fi
+  new_commit=git commit-tree $tree -m 'new version' $parent_opt
+  git update-ref deploy $new_commit
 }
 
 checkout_clean_branch() {
@@ -35,3 +46,6 @@ checkout_clean_branch() {
 compile() {
   NANOC_ENV=production nanoc compile
 }
+
+
+main
